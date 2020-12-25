b4236d35d7c6e6d919ef575c49c008d886ce878b
Tri Nguyen <mail@trimn.net>
2020-12-25 23:59:16 +0100

    Initial Commit

---
+++ Shared/coffeetimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeetimeApp.swift
+//  Shared
+//
+//  Created by Tri Nguyen on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct coffeetimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
